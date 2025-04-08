0fe0bae6af6efd987bd0ed199eeccd2b4e24117a
Mityo133 <125345783+Mityo133@users.noreply.github.com>
2025-04-08 17:55:11 +0300

    Update ApplicationDbContext.cs

--- Movies/Data/ApplicationDbContext.cs
+++ Movies/Data/ApplicationDbContext.cs
@@ -14,6 +14,5 @@ namespace Movies.Data
         public DbSet<Movies.Models.Genres> Genres { get; set; } = default!;
         public DbSet<Movies.Models.Movie> Movie { get; set; } = default!;
         public DbSet<Movies.Models.MovieActors> MovieActors { get; set; } = default!;
-        public DbSet<Movies.Models.Studio> Studios { get; set; } = default!;
     }
 }
