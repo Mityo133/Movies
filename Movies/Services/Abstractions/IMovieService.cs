ccf83a962afab6691295166b79c40859d734ba0c
Niki <Niki@DESKTOP-5M8QMI1>
2025-05-09 18:31:03 +0300

    added tests

---
+++ Movies/Services/Abstractions/IMovieService.cs
@@ -0,0 +1,22 @@
+﻿using Movies.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movies.Data;
+using System.Linq;
+namespace Movies.Services.Abstractions
+{
+     public interface IMovieService
+    {
+        // Define the methods that will be implemented in the MovieService class
+        Task<IEnumerable<Movie>> AllMoviesAsync { get; }
+
+        Task AddMovieAsync(Movie movie);
+        Task UpdateMovieAsync(Movie movie);
+        Task DeleteMovieAsync(int id);
+        Task<IEnumerable<Movie>> SearchMoviesAsync(string searchString);
+        Task<IEnumerable<Movie>> FilterMoviesByYearAsync(int year);
+        Task<IEnumerable<Movie>> SortMoviesAsync(string sortOption);
+    }
+}
---
+++ Movies/Services/MovieService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movies.Data;
+using Movies.Models;
+using Movies.Services.Abstractions;
+
+namespace Movies.Services
+{
+    public class MovieService : IMovieService
+    {
+
+        public Task<IEnumerable<Movie>> AllMoviesAsync => throw new NotImplementedException();
+
+        public Task AddMovieAsync(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovieAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> FilterMoviesByYearAsync(int year)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> SearchMoviesAsync(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> SortMoviesAsync(string sortOption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateMovieAsync(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+    
+}
