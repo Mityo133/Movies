a50514dce1dd45cd626272e6322f1091a0af66a9
nikolai2501 <125345883+nikolai2501@users.noreply.github.com>
2025-05-11 08:31:37 +0300

    Update UnitTest1.cs

--- UnitTestMovies/UnitTest1.cs
+++ UnitTestMovies/UnitTest1.cs
@@ -1,14 +1,40 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-
-namespace UnitTestMovies
+namespace Movies.Tests
 {
-    [TestClass]
-    public class UnitTest1
+    public class MoviesControllerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public async Task AddMovie_ShouldAddMovieSuccessfullyAsync()
         {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var moviesService = new Movies.Services.MoviesService(context);
+                var controller = new MoviesController(moviesService);
+
+                var movie = new Movie
+                {
+                    Name = "Test Movie",
+                    ReleaseYear = 2023,
+                    Description = "Test Description",
+                    Ratings = 5,
+                    GenreId = 1,
+                    Image = "test.jpg",
+                    Trailer = "test.mp4",
+                    Views = 100
+                };
+
+                // Act
+                var result = await controller.Create(movie);
+
+                // Assert
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("Index", ((RedirectToActionResult)result).ActionName);
+            }
         }
+        
     }
 }
